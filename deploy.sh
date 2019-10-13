8754bbaa92b49b4bff0b080826907b4c55ee024b
Luana Gouvea <luanadotcom@gmail.com>
2019-10-13 13:45:33 +0100

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 set -e
 
-
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project
+# Deploy changes to submodule repo
 hugo
-
 cd luanadotcom.github.io
+git add -A
 
-git add .
-
-message="Rebuild site `date`"
-
-if [ -n "$*" ]; then
-    message="$*"
-fi
-
+message="Rebuild site `date +%Y-%m-%d`"
+if [ -n "$1" ]; then
+    message="$1"
+    fi
 git commit -m "$message"
-
 git push origin master
 
-
+# Deploy changes to this repo
+cd ..
+git add -A
+git commit -m "$message"
+git push origin master
